df66df626012bab0cf6a5887e7f11ca64ca9d02c
loicgasser <loicgasser4@gmail.com>
2016-06-03 17:55:14 +0200

    Exclude doc from pypi module

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ import multiprocessing
 from setuptools import setup, find_packages
 
 setup(name='quantized-mesh-tile',
-      version='0.0.1',
+      version='0.1.1',
       description='Quantized-Mesh format reader and writer',
       author='Loic√c Gaer',
       author_email='loicgasser4@gmail.com',
       license='MIT',
       url='https://github.com/loicgasser/quantized-mesh-tile',
-      packages=find_packages(exclude=['tests']),
+      packages=find_packages(exclude=['tests', 'doc']),
       zip_safe=False,
       test_suite='nose.collector',
       install_requires=['shapely', 'numpy'],
