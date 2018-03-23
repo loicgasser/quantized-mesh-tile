7c26f81162de9b40f117c3dc9cf3a49c2ae06a1b
thiloSchlemmer <thilo.schlemmer@gmail.com>
2018-03-23 19:12:47 +0100

    'Refactorings'

--- quantized_mesh_tile/editable_terrain.py
+++ quantized_mesh_tile/editable_terrain.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from future.utils import old_div
+from quantized_mesh_tile.utils import triangleArea
+
+from quantized_mesh_tile.llh_ecef import LLH2ECEF
 
 from quantized_mesh_tile import TerrainTile
 from quantized_mesh_tile.terrain import MAX
+from . import cartesian3d as c3d
 
+null_normal = [0,0,0]
 
 class EditableTerrainTile(TerrainTile):
 
@@ -15,12 +20,10 @@ class EditableTerrainTile(TerrainTile):
                 'south': self._south}
 
     def set_normal(self, index, normal):
-        current_size = len(self.vLight)
-        if current_size <= index:
-            diff = index - current_size
-            temp = [[0, 0, 0] for i in range(diff+1)]
-            self.vLight = self.vLight + temp
         self.vLight[index] = normal
+
+    def set_h(self, index, h):
+        self.h[index] = h
 
     def set_height(self, index, height):
         self.h[index] = self.quantize_height(height)
@@ -47,7 +50,7 @@ class EditableTerrainTile(TerrainTile):
 
     def build_triangles(self):
         indices = iter(self.indices)
-        for i in xrange(0, len(self.indices) - 1, 3):
+        for i in range(0, len(self.indices) - 1, 3):
             vi1 = next(indices)
             vi2 = next(indices)
             vi3 = next(indices)
@@ -55,35 +58,37 @@ class EditableTerrainTile(TerrainTile):
             self._triangles.append(triangle)
 
     def split_triangle(self, triangle_index, vertex_prev_index, vertex_next_index, vertex_insert):
+
         old_triangle = list(self._triangles[triangle_index])
         new_triangle = list(old_triangle)
 
-        # neuen Vertex von lat, lon, height in u,v, h umrechnen
+        # calculate new vertex from lat, lon, height to u,v, h
         u_new, v_new, h_new = self.quantize_vertex(vertex_insert)
 
-        # neuen Vertex hinzufügen in u,v,h
+        # insert new vertex in u,v,h
         self.u.append(u_new)
         self.v.append(v_new)
         self.h.append(h_new)
+        self.vLight.append(null_normal)
+
+        # insert new vertex in lat, lon, height
+        self._longs.append(vertex_insert[0])
+        self._lats.append(vertex_insert[1])
+        self._heights.append(vertex_insert[2])
 
         vertex_new_index = len(self.h) - 1
 
-        # Triangle mit neuem Vertex-Index aktualisieren
+        # update triangle with new vertex index
         old_triangle[old_triangle.index(vertex_next_index)] = vertex_new_index
-        # neues Triangle mit vertex_insert bilden
+        # create new triangle with 'vertex_insert'
         new_triangle[new_triangle.index(vertex_prev_index)] = vertex_new_index
 
-        # neues triangle den Indices hinzufügen
+        # add new triangle to indices-Array
         self.indices.extend(new_triangle)
 
         self._triangles[triangle_index] = old_triangle
-        # neues triangle _triangles hinzufügen
         self._triangles.append(new_triangle)
 
-        if len(self._longs) != 0:
-            self._longs = []
-            self._lats = []
-            self._heights = []
         return vertex_new_index
 
     def quantize_vertex(self, vertex):
@@ -112,3 +117,16 @@ class EditableTerrainTile(TerrainTile):
             b_height = old_div(MAX, deniv)
             h = int(round((height - self.header['minimumHeight']) * b_height))
         return h
+
+    def calculate_normals_for(self,triangles):
+        weighted_normals = []
+        for triangle in triangles:
+            v0 = LLH2ECEF(self._longs[triangle[0]], self._lats[triangle[0]], self._heights[triangle[0]])
+            v1 = LLH2ECEF(self._longs[triangle[1]], self._lats[triangle[1]], self._heights[triangle[1]])
+            v2 = LLH2ECEF(self._longs[triangle[2]], self._lats[triangle[2]], self._heights[triangle[2]])
+
+            normal = np.cross(c3d.subtract(v1, v0), c3d.subtract(v2, v0))
+            area = triangleArea(v0, v1)
+
+            weighted_normals.append(normal * area)
+        return weighted_normals
